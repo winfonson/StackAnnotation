131c679ec4e7298bd937af156506f1da8f104a82
winfonson <41123529+winfonson@users.noreply.github.com>
2020-04-28 15:27:31 +0800

    fail the test case

--- src/stack/MyStringStack.java
+++ src/stack/MyStringStack.java
@@ -22,10 +22,6 @@ public class MyStringStack {
     public void push (String newElement) {
         if (newElement != null) v.add (newElement);
     }
-	
-	MyStringStack stack = new MyStringStack();
-	stack.push (s1);
-	assertTrue (stack.isEmpty());
 /**
      * Returns a String object that was pushed the last on the stack.
      * <p>This method always returns a valid String. </p>
